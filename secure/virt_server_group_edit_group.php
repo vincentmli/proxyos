99c55821c93fbe1c6384037763358ef85414388e
Vincent Li <vincent.mc.li@gmail.com>
2014-08-25 17:54:16 -0700

    add untracked virtual_server_group and introduction page

---
+++ secure/virt_server_group_edit_group.php
@@ -0,0 +1,181 @@
+<?php
+        $selected_host = $_GET['selected_host'];
+	$vev_action = "";
+
+	if (isset($_GET['vev_action'])) {
+		$vev_action = $_GET['vev_action'];
+	}
+
+	if ($vev_action == "CANCEL") {
+		/* Redirect browser to editing page */
+		header("Location: virt_server_group_main.php?selected_host=$selected_host");
+		/* Make sure that code below does not get executed when we redirect. */
+		exit;
+	}
+
+	if (($selected_host == "")) {
+		header("Location: virt_server_group_main.php");
+		exit;
+	}
+
+	if ($vev_action == "EDIT") {
+		/* Redirect browser to editing page */
+		header("Location: virtual_edit_services.php?selected_host=$selected_host");
+		/* Make sure that code below does not get executed when we redirect. */
+		exit;
+	}
+	
+	/* try and make this page non cacheable */
+	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");             // Date in the past
+	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");// always modified
+	header("Cache-Control: no-cache, must-revalidate");           // HTTP/1.1
+	header("Pragma: no-cache");                                   // HTTP/1.0
+
+	require('parse.php'); /* read in the config! Hurragh! */
+
+
+	if ( $vev_action == "ACCEPT" ) {
+
+		$virt_server_group[$selected_host]['virt_server_group'] 	= $_GET['virt_server_group']; 
+	}
+
+?>
+<HTML>
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML Strict Level 3//EN">
+
+<HEAD>
+<script language="javascript" type="text/javascript" src="jquery-1.11.0.js"></script>
+<script language="javascript" type="text/javascript" src="jquery.validate.js"></script>
+<script language="javascript" type="text/javascript" src="superez.js"></script>
+
+<TITLE>Piranha (VRRP Instances - Editing VRRP instance)</TITLE>
+
+<STYLE TYPE="text/css">
+<!-- 
+
+TD      {
+        font-family: helvetica, sans-serif;
+        }
+        
+.logo   {
+        color: #FFFFFF;
+        }
+        
+A.logolink      {
+        color: #FFFFFF;
+        font-size: .8em;
+        }
+        
+.taboff {
+        color: #FFFFFF;
+        }
+        
+.tabon  {
+        color: #999999;
+        }
+        
+.title  {
+        font-size: .8em;
+        font-weight: bold;
+        color: #660000;
+        }
+        
+.smtext {
+        font-size: .8em;
+        }
+        
+.green  {
+        color: 
+	}
+// -->
+</STYLE>
+
+</HEAD>
+
+<BODY BGCOLOR="#660000">
+
+<TABLE WIDTH="100%" BORDER="0" CELLSPACING="0" CELLPADDING="5">
+	<TR BGCOLOR="#CC0000"> <TD CLASS="logo"> <B>KEEPALIVED</B> CONFIGURATION TOOL </TD>
+	<TD ALIGN=right CLASS="logo">
+            <A HREF="introduction.php" CLASS="logolink">
+            INTRODUCTION</A> | <A HREF="help.php" CLASS="logolink">
+            HELP</A></TD>
+	</TR>
+</TABLE>
+
+<TABLE WIDTH="100%" BORDER="0" CELLSPACING="0" CELLPADDING="5">
+        <TR>
+            <TD>&nbsp;<BR><FONT SIZE="+2" COLOR="#CC0000">EDIT VIRTUAL SERVER GROUP</FONT><BR>&nbsp;</TD>
+        </TR>
+</TABLE>
+
+<TABLE WIDTH="100%" BORDER="0" CELLSPACING="0" CELLPADDING="0"><TR><TD BGCOLOR="#FFFFFF">
+
+<?php include 'menu.php'; ?>
+
+<?php
+	// echo "Query = $QUERY_STRING";
+
+?>
+
+<?php if ($prim['service'] == "fos") { ?>
+<TABLE WIDTH="100%" BORDER="0" CELLSPACING="0" CELLPADDING="5">
+        <TR BGCOLOR="#EEEEEE">
+                <TD WIDTH="60%">EDIT:
+		
+		<A HREF="vrrp_edit_vrrp.php<?php if (!empty($selected_host)) { echo "?selected_host=$selected_host"; } ?> " CLASS="tabon" NAME="VRRP instance">VRRP INSTANCE</A>
+		&nbsp;|&nbsp;
+
+                <A HREF="virtual_edit_services.php<?php if (!empty($selected_host)) { echo "?selected_host=$selected_host"; } ?> " NAME="MONITORING SCRIPTS">MONITORING SCRIPTS</A></TD>
+
+        </TR>
+</TABLE>
+
+<?php } else { ?>
+<TABLE WIDTH="100%" BORDER="0" CELLSPACING="0" CELLPADDING="5">
+        <TR BGCOLOR="#EEEEEE">
+                <TD WIDTH="60%">EDIT:
+		
+		<A HREF="virt_server_group_edit_group.php<?php if (!empty($selected_host)) { echo "?selected_host=$selected_host"; } ?> " CLASS="tabon" NAME="VIRT server group">VIRTUAL SERVER GROUP</A>
+		&nbsp;|&nbsp;
+
+                <A HREF="virt_server_group_edit_iprange.php<?php if (!empty($selected_host)) { echo "?selected_host=$selected_host"; } ?> " NAME="IPRANGE">IPRANGE</A>
+		&nbsp;|&nbsp;
+
+                <A HREF="virt_server_group_edit_fwmark.php<?php if (!empty($selected_host)) { echo "?selected_host=$selected_host"; } ?> " NAME="FWMARK">FWMARK</A>
+		&nbsp;|&nbsp;
+
+
+        </TR>
+</TABLE>
+<?php } ?>
+
+<FORM METHOD="GET" id="virt_server_group_form" ENCTYPE="application/x-www-form-urlencoded" ACTION="virt_server_group_edit_group.php">
+<TABLE>
+	<TR>
+		<TD>Virtual server group name:</TD>
+		<TD><INPUT TYPE="TEXT" NAME="virt_server_group" VALUE= <?php echo $virt_server_group[$selected_host]['virt_server_group'] ; ?>></TD>
+	</TR>
+
+
+</TABLE>
+<?php echo "<INPUT TYPE=HIDDEN NAME=selected_host VALUE=$selected_host>" ?>
+
+<TABLE WIDTH="100%" BORDER="0" CELLSPACING="1" CELLPADDING="5">
+        <TR BGCOLOR="#666666">
+                <TD>
+                        <INPUT TYPE="Submit" NAME="vev_action" VALUE="ACCEPT">  
+                        <SPAN CLASS="taboff">-- Click here to apply changes to this page</SPAN>
+                </TD>
+                <TD>
+                        <INPUT TYPE="Submit" NAME="vev_action" VALUE="CANCEL">  
+                        <SPAN CLASS="taboff">-- Click here to cancel the changes</SPAN>
+                </TD>
+        </TR>
+</TABLE>
+
+<?php open_file ("w+"); write_config(""); ?>
+</FORM>
+</TD></TR></TABLE>
+</BODY>
+</HTML>
